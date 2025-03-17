590fcc1d2e5124f2b29c79169c940ac12f4e87a6
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-03-17 11:44:54 +0300

    Updating miso and the extension

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const ramens = [
     { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "./images/shoyu.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "./images/miso.jpeg", rating: 4, comment: "Very flavorful!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "./images/miso.jpg", rating: 4, comment: "Very flavorful!" },
     { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "./images/tonkotsu.jpg" }
  ];
 
@@ -82,3 +82,5 @@ function handleDisplay(event) {
         document.querySelector('#ramen-comment').textContent = `Comment: ${selectedRamen.comment ? selectedRamen.comment : "No comment available"}`;
     }
 }
+
+
